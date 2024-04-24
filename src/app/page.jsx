4ab627bc24af818a7e9d4fa2014869f8595efde7
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-04-24 16:02:40 +0530

    maked text bold

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,18 @@ const page = () => {
   return (
     <>
       <div className="mt-20 lg:w-[44rem]">
-        <h1 className="text-3xl md:text-5xl text-zinc-500">i'm jishan</h1>
-        <h1 className="text-4xl md:text-5xl">
+        <h1 className="font-bold text-3xl md:text-5xl text-zinc-500">
+          i'm jishan
+        </h1>
+        <h1 className="text-4xl font-bold md:text-5xl">
           Transforming client visions into vibrant digital realities
         </h1>
       </div>
-      <div data-scroll data-scroll-speed={0.1}>
+      <div
+        className="hidden md:block lg:block"
+        data-scroll
+        data-scroll-speed={0.1}
+      >
         <Rounded className="w-40 h-40 bg-[#1C1D20] text-white rounded-full flex items-center top-80 right-1/4 justify-center cursor-pointer absolute">
           <p className="m-0 text-base z-10 relative">About me</p>
         </Rounded>
