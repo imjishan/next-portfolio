d3f0b4ce8323980f40a1bc20872d788a125dab8b
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-04-29 15:54:59 +0530

    minor changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,6 @@
-import Aboutbutton from "@/components/AboutmeButton/Aboutbutton";
 import Desc from "@/components/Description/Desc";
 import Hero from "@/components/Hero/Hero";
 import { Separator } from "@/components/ui/separator";
-
 const page = () => {
   return (
     <>
